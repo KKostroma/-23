5936344c6e5e473b7cfd4af71b1906cf6a2db007
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2022-01-26 20:55:16 +0300

    Финализация

--- Задание23/Program.cs
+++ Задание23/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Задание23
@@ -10,6 +11,24 @@ namespace Задание23
     {
         static void Main(string[] args)
         {
+            int n = Convert.ToInt32(Console.ReadLine());
+            FactAsync(n);
+            Console.ReadKey();
+        }
+
+        static void Fact(int n)
+        {
+            int f = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                f = f * i;
+                Thread.Sleep(10);
+            }
+            Console.WriteLine(f);
+        }
+        static async void FactAsync(int n)
+        {
+            await Task.Run(()=>Fact(n));
         }
     }
 }
